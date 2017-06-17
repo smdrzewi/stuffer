d90bdc05d4cd7530eee7d7d3875de1192caea4d9
sssssss340 <viis340@gmail.com>
2017-06-17 08:48:55 -0400

    v1.0.0

--- stuffer.py
+++ stuffer.py
@@ -1,26 +1,67 @@
-#!/usr/bin/python
+#!/usr/bin/env python2.7
 
 import logging
-#logging.getLogger("scapy").setLevel(logging.DEBUG)
-
+import argparse
+import socket
+import sys
 from scapy.all import *
 
-def sendall(message):
-	m = IP()/TCP()
-	m.dst = "172.16.208.169"
-	m[TCP].reserved = 111
-	pad = Padding()
-	pad.load = message
-	m = m/pad
-	send(m)
-	print("Sent %s\n" % message)
-	return
+def Send():
+	while True:
+		inp = None
+		logger.info('0 to exit')
+		while inp == None and not inp == '0':
+			inp = str(raw_input('Enter Data: '))
+		if inp == '0':
+			sys.exit()
+		m = IP()/TCP()
+		pad = Padding()
+		pad.load = inp
+		while len(pad.load) < (60-len(m)):
+			pad.load += '\x00'
+		m = m/pad
+			
+		m.dst = args.target
+		sr1(m)
+		m.show2()
 
+def declutter(p):
+	if (p[IP].src == args.target):
+		return p[IP].src
 
-print("----------------------")
-print("| Welcome to Stuffer |")
-print("----------------------")
-print("By: Sean Drzewiecki &")
-print("    Aaron Gudrian")
-while True:
-	sendall(str(raw_input("Enter stuffing: ")))
+def Receive():
+	logger.info('Awaiting Packets')
+	sniff(filter="tcp", prn=declutter)
+
+parser = argparse.ArgumentParser()
+exclusive = parser.add_mutually_exclusive_group()
+exclusive.add_argument('--send', '-s', help='Send data to the target', action = 'store_true')
+exclusive.add_argument('--receive', '-r', help='Receive data from the target', action = 'store_true')
+parser.add_argument('target', help='Destination Server')
+parser.add_argument('--verbose', '-v', help='Enable verbose logging', action='store_true')
+
+args = parser.parse_args()
+
+logger = logging.getLogger('Stuffer')
+handler = logging.StreamHandler()
+handler.setLevel(logging.DEBUG)
+formatter = logging.Formatter('{0}[{1}%(levelname)s{0}] %(message)s'.format('\033[0;49m','\033[1;31m'))
+handler.setFormatter(formatter)
+logger.addHandler(handler)
+if args.verbose:
+	logger.setLevel(logging.DEBUG)
+else:
+	logger.setLevel(logging.INFO)
+
+try:
+	socket.inet_aton(args.target)
+except socket.error as e:
+	logger.critical(e)
+	sys.exit()
+
+if args.send:
+	Send()
+elif args.receive:
+	Receive()
+else:
+	logger.critical('No command selected!')
